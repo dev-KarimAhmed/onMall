f45de07fa5fba43e5600ecae42e257bfe05ae2b6
Karim Ahmed <devKarimAhmed@gmail.com>
2025-02-20 23:43:52 +0200

    add more categories

--- lib/core/styles/images/app_assets.dart
+++ lib/core/styles/images/app_assets.dart
@@ -3,13 +3,22 @@ class AppAssets {
   static const noNetwork = 'assets/images/core/no_network.jpg';
   static const pageUnderBuild = 'assets/images/core/page_under_build.jpg';
   static const emptyScreen = 'assets/images/core/empty_screen.png';
-  static const onboardingImageOne = 'assets/images/core/onboarding_image_one.gif';
-  static const onboardingImageTwo = 'assets/images/core/onboarding_image_two.gif';
+  static const onboardingImageOne =
+      'assets/images/core/onboarding_image_one.gif';
+  static const onboardingImageTwo =
+      'assets/images/core/onboarding_image_two.gif';
   static const shoping = 'assets/images/core/shopping.gif';
   static const jackets = 'assets/images/icons/jacket.png';
   static const pants = 'assets/images/icons/jeans.png';
   static const shoes = 'assets/images/icons/running-shoe.png';
   static const tshirt = 'assets/images/icons/tshirt.png';
   static const underwear = 'assets/images/icons/underwear.png';
-
+  static const hoodie = 'assets/images/icons/hoodie.png';
+  static const sweater = 'assets/images/icons/sweater.png';
+  static const cap = 'assets/images/icons/cap.png';
+  static const dress = 'assets/images/icons/dress.png';
+  static const shorts = 'assets/images/icons/shorts.png';
+  static const skirt = 'assets/images/icons/skirt.png';
+  static const socks = 'assets/images/icons/socks.png';
+  static const suit = 'assets/images/icons/suit.png';
 }
--- lib/features/home/presentation/views/widgets/categories_list.dart
+++ lib/features/home/presentation/views/widgets/categories_list.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:on_mall/core/functions/get_color.dart';
 import 'package:on_mall/core/functions/get_text_style.dart';
+import 'package:on_mall/core/functions/translate_word.dart';
+import 'package:on_mall/core/language/lang_keys.dart';
 import 'package:on_mall/core/styles/images/app_assets.dart';
 
 class CategoriesList extends StatelessWidget {
@@ -15,7 +17,7 @@ class CategoriesList extends StatelessWidget {
       height: 100.h,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: categories.length,
+        itemCount: categories(context).length,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
@@ -27,11 +29,11 @@ class CategoriesList extends StatelessWidget {
                     radius: 30,
                     backgroundColor: getColors(context).primaryColor,
                     foregroundColor: getColors(context).mainColor,
-                    child: categories[index].icon,
+                    child: categories(context)[index].icon,
                   ),
                   const RSizedBox(height: 5),
                   Text(
-                    categories[index].text,
+                    categories(context)[index].text,
                     style: getMediumTextStyle(context),
                   ),
                 ],
@@ -44,25 +46,76 @@ class CategoriesList extends StatelessWidget {
   }
 }
 
-
-//TODO : Translate them
-List<Category> categories = [
-  Category(
-      icon: const ImageIcon(
-
-        AssetImage(
-          AppAssets.tshirt,
-        ),
-      ),
-      text: "T-Shirt"),
-  Category(
-      icon: const ImageIcon(AssetImage(AppAssets.jackets)), text: "Jacket"),
-  Category(icon: const ImageIcon(AssetImage(AppAssets.pants)), text: "Pants"),
-  Category(icon: const ImageIcon(AssetImage(AppAssets.shoes)), text: "Shoes"),
-  Category(
-      icon: const ImageIcon(AssetImage(AppAssets.underwear)),
-      text: "Men-Underwear"),
-];
+List<Category> categories(BuildContext context) => [
+      Category(
+          icon: const ImageIcon(
+            AssetImage(
+              AppAssets.tshirt,
+            ),
+          ),
+          text: translateWord(context, Langkeys.tshirt)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.jackets),
+          ),
+          text: translateWord(context, Langkeys.jackets)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.pants),
+          ),
+          text: translateWord(context, Langkeys.pants)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.shoes),
+          ),
+          text: translateWord(context, Langkeys.shoes)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.underwear),
+          ),
+          text: translateWord(context, Langkeys.menUnderwear)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.hoodie),
+          ),
+          text: translateWord(context, Langkeys.hoodie)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.sweater),
+          ),
+          text: translateWord(context, Langkeys.sweater)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.cap),
+          ),
+          text: translateWord(
+              context, Langkeys.hat)), // Changed from "Cap" to "Hat"
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.dress),
+          ),
+          text: translateWord(context, Langkeys.dress)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.shorts),
+          ),
+          text: translateWord(context, Langkeys.shorts)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.skirt),
+          ),
+          text: translateWord(context, Langkeys.skirt)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.socks),
+          ),
+          text: translateWord(context, Langkeys.socks)),
+      Category(
+          icon: const ImageIcon(
+            AssetImage(AppAssets.suit),
+          ),
+          text: translateWord(context, Langkeys.suit)),
+    ];
 
 class Category {
   final Widget icon;
